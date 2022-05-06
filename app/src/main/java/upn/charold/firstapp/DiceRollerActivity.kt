ef99bb0db09ed75c0c29f9253dd7cce36d2a210c
charold-legion-s7 <harolrop14@gmail.com>
2022-05-05 19:28:57 -0500

    limpieza y formateo de codigo en dicerolleractv

--- app/src/main/java/upn/charold/firstapp/DiceRollerActivity.kt
+++ app/src/main/java/upn/charold/firstapp/DiceRollerActivity.kt
@@ -7,6 +7,10 @@ import android.widget.TextView
 import android.widget.Toast
 import upn.charold.firstapp.model.Dice
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class DiceRollerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -16,15 +20,33 @@ class DiceRollerActivity : AppCompatActivity() {
         this.setTitle("DiceRollerApp")
 
         val rollButton: Button = findViewById(R.id.roll_btn)
-        rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
 
-            val resultTextView: TextView = findViewById(R.id.rollNumber_txt)
-            val dice1 = Dice(6)
-            resultTextView.text = dice1.roll().toString()
+        rollButton.setOnClickListener {
+            toast()
+            rollDice()
         }
 
+    }
+
+    /**
+     * Create toast for dice roll.
+     */
+    private fun toast() {
+        val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.rollNumber_txt)
+        resultTextView.text = diceRoll.toString()
     }
 
 }
--- app/src/main/java/upn/charold/firstapp/model/Dice.kt
+++ app/src/main/java/upn/charold/firstapp/model/Dice.kt
@@ -1,6 +1,7 @@
 package upn.charold.firstapp.model
 
 class Dice(private val numSides: Int){
+
     fun roll(): Int{
         return (1..numSides).random()
     }
