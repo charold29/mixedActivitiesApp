3697ad5fa16ae0f2b08bfbbfd7a5c604316060a4
charold-legion-s7 <harolrop14@gmail.com>
2022-05-05 19:16:30 -0500

    dice class added and instance in diceRollerAct

--- app/src/main/java/upn/charold/firstapp/DiceRollerActivity.kt
+++ app/src/main/java/upn/charold/firstapp/DiceRollerActivity.kt
@@ -3,7 +3,9 @@ package upn.charold.firstapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import upn.charold.firstapp.model.Dice
 
 class DiceRollerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +19,10 @@ class DiceRollerActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
             toast.show()
+
+            val resultTextView: TextView = findViewById(R.id.rollNumber_txt)
+            val dice1 = Dice(6)
+            resultTextView.text = dice1.roll().toString()
         }
 
     }
