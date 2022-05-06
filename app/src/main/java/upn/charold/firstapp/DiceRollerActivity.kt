05d85998714e138a2eaca2e3d9fc4c9c500bc661
charold-legion-s7 <harolrop14@gmail.com>
2022-05-05 19:02:54 -0500

    toast en rollButton

--- app/src/main/java/upn/charold/firstapp/DiceRollerActivity.kt
+++ app/src/main/java/upn/charold/firstapp/DiceRollerActivity.kt
@@ -2,12 +2,23 @@ package upn.charold.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class DiceRollerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dice_roller)
+
         this.setTitle("DiceRollerApp")
+
+        val rollButton: Button = findViewById(R.id.roll_btn)
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
     }
 
 }
